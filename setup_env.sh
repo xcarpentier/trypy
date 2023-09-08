b14c979009d441f80b98b646b175cd27d5c6d1b4
Xavier Carpentier <xcapetir@gmail.com>
2023-09-08 11:23:14 +0200

    create setup_env.sh

---
+++ setup_env.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+# This script is used to setup the environment for the project.
+
+# if virtual environment is activate then exit.
+if [ -n "$VIRTUAL_ENV" ]; then
+    echo "Virtual environment is already activated."
+    exit 0
+fi
+
+# Check if .venv exists, if not create it.
+if [ ! -d .venv ]; then
+    python3 -m venv .venv
+fi
+
+# Activate the virtual environment.
+source .venv/bin/activate`
+
+# Install the requirements.
+pip install -r requirements.txt
--- test_sample.py
+++ test_sample.py
@@ -1,16 +1,26 @@
 def sum():
     x = 0
+    y = []
+    z = []
     for i in range(10):
-        x+=i
-        if x % 2:
-            print("x")
-    return x
+        z.append(i)
+        if i % 2 == 0:
+            y.append(i)
+            x += i
+    return (x, y, z)
+
 
 def inc(x):
     return x + 1
 
+
 def test_answer_1():
     assert inc(3) == 4
 
+
 def test_answer_2():
-    assert sum() == 45
+    assert sum() == (
+        20,
+        [0, 2, 4, 6, 8],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    )
