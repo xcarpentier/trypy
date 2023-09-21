73bee2614c9d8cb6938eae7e25815d941ce7b943
Xavier Carpentier <xcapetir@gmail.com>
2023-09-21 13:45:37 +0200

    add bound_sqrt_2

--- Calculs.py
+++ Calculs.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def is_multiple(a: int, b: int):
     return a % b == 0
 
@@ -14,3 +17,15 @@ def is_prime(n: int):
             return False
         d += 1
     return True
+
+
+def bound_sqrt_2(digit: int) -> List[float]:
+    val = 2
+    step = 10**-digit
+    x = 0
+    y = 0
+    while (x + step) ** 2 < val:
+        x += step
+    while y**2 < val:
+        y += step
+    return [round(x, digit), round(y, digit)]
--- Calculs_test.py
+++ Calculs_test.py
@@ -1,4 +1,5 @@
-from Calculs import is_multiple, is_bigest_multiple, is_prime
+from Calculs import is_multiple, is_bigest_multiple, is_prime, bound_sqrt_2
+from math import sqrt, pow
 
 
 def test_is_multiple():
@@ -14,8 +15,16 @@ def test_is_biggest_multiple():
 
 
 def test_is_prime():
-    assert is_prime(7)
+    assert is_prime(108877)
 
 
 def test_is_not_prime():
-    assert not is_prime(12)
+    assert not is_prime(123456789991)
+
+
+def test_bound_sqrt_2_dyn():
+    for x in range(3):
+        assert bound_sqrt_2(x) == [
+            round(sqrt(2), x),
+            round(round(sqrt(2), x) + pow(10, -x), x),
+        ]
